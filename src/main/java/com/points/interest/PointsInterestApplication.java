77baa3adf9429eeaa0f49d49cae17d5061883329
Red Panda <arthurstephan13@gmail.com>
2023-10-06 23:38:13 -0300

    Iniciando projeto

---
+++ src/main/java/com/points/interest/PointsInterestApplication.java
@@ -0,0 +1,13 @@
+package com.points.interest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsInterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/points/interest/PointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.points.interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
